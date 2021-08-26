4629cfd4d6438525fb5669708922a57e676b6cba
Wenbo Zhu <wwenbo@email.unc.edu>
2021-08-25 21:37:47 -0400

    ex01.1 uploaded

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,9 @@
 # TODO: Write docstring here
-
+"""hype is real"""
 # TODO: Initialize __author__ variable here
-
+__author__ = "730444340"
 # TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print(name + " is cool!")
+print("Hang in there " + name + "!")
+print("You know what " + name + "… it's a leap of faith.")
