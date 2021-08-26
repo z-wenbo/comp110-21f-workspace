a1e31fb58f1e781ac75e3adfa82c1992972367c1
Wenbo Zhu <wwenbo@email.unc.edu>
2021-08-25 22:09:38 -0400

    upload rest of ex01

---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,11 @@
+
+"""trying out some numeric operators"""
+
+__author__ = "730444340"
+
+left: int = int(input("Left-hand side: "))
+right: int = int(input("Right-hand side: "))
+print(str(left) + " ** " + str(right) + " is " + str(left**right))
+print(str(left) + " / " + str(right) + " is " + str(left / right))
+print(str(left) + " // " + str(right) + " is " + str(left // right))
+print(str(left) + " % " + str(right) + " is " + str(left % right))
---
+++ exercises/ex01/relational_operators.py
@@ -0,0 +1,10 @@
+"""trying out some relational operators"""
+
+__author__ = "730444340"
+
+left: int = int(input("Left-hand side: "))
+right: int = int(input("Right-hand side: "))
+print(str(left) + " < " + str(right) + " is " + str(left < right))
+print(str(left) + " >= " + str(right) + " is " + str(left >= right))
+print(str(left) + " == " + str(right) + " is " + str(left == right))
+print(str(left) + " != " + str(right) + " is " + str(left != right))
